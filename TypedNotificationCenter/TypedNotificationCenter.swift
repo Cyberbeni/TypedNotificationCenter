1fdcc706b5be826aa027344f5c7f60b4d655146f
Benedek Kozma <cyberbeni@gmail.com>
2019-05-05 15:01:57 +0200

    Initial Commit

---
+++ TypedNotificationCenter/TypedNotificationCenter.swift
@@ -0,0 +1,11 @@
+//
+//  TypedNotificationCenter.swift
+//  TypedNotificationCenter
+//
+//  Created by Benedek Kozma on 2019. 05. 05..
+//  Copyright © 2019. Benedek Kozma. All rights reserved.
+//
+
+class TypedNotificationCenter {
+
+}
