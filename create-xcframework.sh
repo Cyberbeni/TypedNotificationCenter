163307b85f40f465165e96f2acbf0084d7299587
Benedek Kozma <cyberbeni@gmail.com>
2022-04-30 00:53:46 +0200

    iterate by index instead of platform

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -14,44 +14,52 @@ mkdir -p "$ARCHIVES_ROOT_PATH"
 
 CREATE_XCFRAMEWORK_ARGUMENTS=()
 
-for PLATFORM in "macOS" "macOS,variant=Mac Catalyst" "iOS" "iOS Simulator" "tvOS" "tvOS Simulator" "watchOS" "watchOS Simulator"; do
-	case $PLATFORM in
-	"macOS")
-	ARCHIVE_NAME="Release-macos"
-	SCHEME="TypedNotificationCenter macOS"
+for idx in {1..8}; do
+	case $idx in
+	1)
+		PLATFORM="macOS"
+		ARCHIVE_NAME="Release-macos"
+		SCHEME="TypedNotificationCenter macOS"
 	;;
-	"macOS,variant=Mac Catalyst")
-	ARCHIVE_NAME="Release-maccatalyst"
-	SCHEME="TypedNotificationCenter iOS"
+	2)
+		PLATFORM="macOS,variant=Mac Catalyst"
+		ARCHIVE_NAME="Release-maccatalyst"
+		SCHEME="TypedNotificationCenter iOS"
 	;;
-	"iOS")
-	ARCHIVE_NAME="Release-iphoneos"
-	SCHEME="TypedNotificationCenter iOS"
+	3)
+		PLATFORM="iOS"
+		ARCHIVE_NAME="Release-iphoneos"
+		SCHEME="TypedNotificationCenter iOS"
 	;;
-	"iOS Simulator")
-	ARCHIVE_NAME="Release-iphonesimulator"
-	SCHEME="TypedNotificationCenter iOS"
+	4)
+		PLATFORM="iOS Simulator"
+		ARCHIVE_NAME="Release-iphonesimulator"
+		SCHEME="TypedNotificationCenter iOS"
 	;;
-	"tvOS")
-	ARCHIVE_NAME="Release-tvos"
-	SCHEME="TypedNotificationCenter tvOS"
+	5)
+		PLATFORM="tvOS"
+		ARCHIVE_NAME="Release-tvos"
+		SCHEME="TypedNotificationCenter tvOS"
 	;;
-	"tvOS Simulator")
-	ARCHIVE_NAME="Release-tvossimulator"
-	SCHEME="TypedNotificationCenter tvOS"
+	6)
+		PLATFORM="tvOS Simulator"
+		ARCHIVE_NAME="Release-tvossimulator"
+		SCHEME="TypedNotificationCenter tvOS"
 	;;
-	"watchOS")
-	ARCHIVE_NAME="Release-watchos"
-	SCHEME="TypedNotificationCenter watchOS"
+	7)
+		PLATFORM="watchOS"
+		ARCHIVE_NAME="Release-watchos"
+		SCHEME="TypedNotificationCenter watchOS"
 	;;
-	"watchOS Simulator")
-	ARCHIVE_NAME="Release-watchsimulator"
-	SCHEME="TypedNotificationCenter watchOS"
+	8)
+		PLATFORM="watchOS Simulator"
+		ARCHIVE_NAME="Release-watchsimulator"
+		SCHEME="TypedNotificationCenter watchOS"
 	;;
 	esac
 
-   ARCHIVE_PATH="$ARCHIVES_ROOT_PATH/$ARCHIVE_NAME.xcarchive"
-   LOG_PATH="$ARCHIVES_ROOT_PATH/$ARCHIVE_NAME.log"
+	ARCHIVE_PATH="$ARCHIVES_ROOT_PATH/$ARCHIVE_NAME.xcarchive"
+	LOG_PATH="$ARCHIVES_ROOT_PATH/$ARCHIVE_NAME.log"
 
 	xcodebuild archive \
 		-project "TypedNotificationCenter.xcodeproj" \
@@ -63,7 +71,7 @@ for PLATFORM in "macOS" "macOS,variant=Mac Catalyst" "iOS" "iOS Simulator" "tvOS
 		SKIP_INSTALL=NO \
 		BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
 		| tee "$LOG_PATH"
-	
+
 	FRAMEWORK_PATH=$(find "$ARCHIVE_PATH" -name "*.framework" -print -quit)
 	if [ -n "$FRAMEWORK_PATH" ]; then
 		CREATE_XCFRAMEWORK_ARGUMENTS+=( "-framework" "$PWD/$FRAMEWORK_PATH" )
