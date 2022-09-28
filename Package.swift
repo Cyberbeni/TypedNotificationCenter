b8208f5406853f1324458568e4bcb8496f21a162
Benedek Kozma <cyberbeni@gmail.com>
2022-09-28 18:03:57 +0200

    update minimum versions with Xcode 14 minimum supported versions

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
 	name: "TypedNotificationCenter",
 	platforms: [
-		.macOS("10.9"),
-		.iOS(.v8),
-		.watchOS(.v2),
-		.tvOS(.v9),
+		.macOS(.v10_13),
+		.iOS(.v11),
+		.watchOS(.v4),
+		.tvOS(.v11),
 	],
 	products: [
 		.library(
